574f948fb1543710f411ed71df584e61353460dc
James Dixon <jim.w.dixon@gmail.com>
2017-04-11 17:57:26 -0600

    initial commit

---
+++ src/components/Widget.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class='panel-body'>
+    <vue-form-generator :schema="schema" :model="model" :options="formOptions"></vue-form-generator>
+  </div>
+</template>
+
+<script>
+  import VueFormGenerator from 'vue-form-generator/dist/vfg-core.js'
+  import 'vue-form-generator/dist/vfg-core.css'
+
+  export default {
+    components: {
+      'vue-form-generator': VueFormGenerator.component
+    },
+
+    data: function () {
+      return {
+
+        model: {
+          id: 1,
+          name: 'John Doe',
+          password: 'J0hnD03!x4',
+          skills: ['Javascript', 'VueJS'],
+          email: 'john.doe@gmail.com',
+          status: true
+        },
+
+        schema: {
+          fields: [{
+            type: 'input',
+            inputType: 'text',
+            label: 'ID',
+            model: 'id',
+            readonly: true,
+            featured: false,
+            disabled: true
+          }, {
+            type: 'input',
+            inputType: 'text',
+            label: 'Name',
+            model: 'name',
+            readonly: false,
+            featured: true,
+            required: true,
+            disabled: false,
+            placeholder: 'Name',
+            validator: VueFormGenerator.validators.string
+          }, {
+            type: 'input',
+            inputType: 'password',
+            label: 'Password',
+            model: 'password',
+            min: 6,
+            required: true,
+            hint: 'Minimum 6 characters',
+            validator: VueFormGenerator.validators.string
+          }, {
+            type: 'input',
+            inputType: 'number',
+            label: 'Age',
+            model: 'age',
+            min: 18,
+            validator: VueFormGenerator.validators.number
+          }, {
+            type: 'input',
+            inputType: 'email',
+            label: 'E-mail',
+            model: 'email',
+            placeholder: 'Email',
+            validator: VueFormGenerator.validators.email
+          }, {
+            type: 'checklist',
+            label: 'Skills',
+            model: 'skills',
+            multi: true,
+            required: true,
+            multiSelect: true,
+            values: ['HTML5', 'Javascript', 'CSS3', 'CoffeeScript', 'AngularJS', 'ReactJS', 'VueJS']
+          }, {
+            type: 'switch',
+            label: 'Status',
+            model: 'status',
+            multi: true,
+            readonly: false,
+            featured: false,
+            disabled: false,
+            default: true,
+            textOn: 'Active',
+            textOff: 'Inactive'
+          }]
+        },
+
+        formOptions: {
+          validateAfterLoad: true,
+          validateAfterChanged: true
+        }
+      }
+    }
+}
+</script>
